2f68841e1cf828785cef33703ca60a3f3eaebf0f
HifiAudi0 <77525153+HifiAudi0@users.noreply.github.com>
2022-12-16 07:59:35 -0500

    MAJOR STABLE timestamping is working and we figured out where mute can/can not play, working nicely at this point

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import { useEffect, useRef, useState } from "react";
 import axios from 'axios';
+import { CLIENT_MULTI_STATEMENTS } from 'mysql/lib/protocol/constants/client';
 
 
 function IntialPage() {
@@ -13,6 +14,7 @@ function IntialPage() {
   const [video_id, setVideoId] = useState("");
 
 
+  // Chrome deveeloper console - highlight object then ctrl+alt+click to expand all object properties
 
   // fetch(`/sendUrl/?url=${props.url}`, {
   //   method: "POST"
@@ -22,7 +24,7 @@ function IntialPage() {
   useEffect(() => {
 
     // Videos with Confirmed swearing in them: 
-    //https://www.youtube.com/watch?v=_SvIzSD0USE&t=76s
+    //https://www.youtube.com/watch?v=_SvIzSD0USE
     // https://www.youtube.com/watch?v=2uvV1-02UCU - ALOT OF SWEARING OVER 300 TIMES I THINK
     // https://www.youtube.com/watch?v=2d4L1flXhLY
 
@@ -59,7 +61,7 @@ function IntialPage() {
       {/*  WORKS - jsonSwearingData = FetchSubmit({ url: document.getElementById("url").value }) */}
       {/* create a form with onsubmit and call the function FetchSubmit passing down the argument document.getElementById("url").value */}
       <form onSubmit={() => { }}>
-        <label htmlFor="url">Youtube URL (example: https://www.youtube.com/watch?v=IhQmXaEhksI) :</label><br />
+        <label htmlFor="url">Youtube URL (example: https://www.youtube.com/watch?v=_SvIzSD0USEI) :</label><br />
         <input className="url" type="text" id="url" name="url" /><br />
         <input type="submit" value="Submit"></input>
       </form>
@@ -124,58 +126,106 @@ function LoadVideo(video_id, data) {
     }
 
     function onPlayerReady(event) {
-      player.loadVideoById(`${video_id["video_id"]}`, 5, "large")
 
+      player.loadVideoById(`${video_id["video_id"]}`, 5, "large")
+      // var duration = player.getDuration();
+      // console.log()
+      // player.playerInfo.apiInterface.seekTo(200, true);
       // event.target.playVideo();
+
+      var currentTimestamp, startSwearing, durationSwearing;
+      var endSwearing = startSwearing + durationSwearing;
+      // console.log("FIRST MUTEEEEEEEEEEEEEEEEEE")
+      // player.mute();
+
+      // console.log("LENGTH", video_id["data"].length)
+
+      // try {
+      //   console.log("CURRENT TIME STAMP IS KOOLAIDKOOLAIDKOOLAIDKOOLAIDKOOLAIDKOOLAID", player.playerInfo.currentTime)
+      // } catch (err) {
+      //   console.log("ERROR IS", err)
+      // }
+
+      // CANT get timestamp here
+      // CAN mute here
+      // player.mute();
+
+
+
+      // setInterval(function () {
+      //   try {
+      //     console.log("CURRENT TIME STAMP IS MOUSEMOUSEMOUSEMOUSEMOUSEMOUSEMOUSE", player.playerInfo.currentTime)
+      //   } catch (err) {
+      //     console.log("ERROR IS", err)
+      //   }
+      //     console.log("PLAYER1111111111", player)
+      //     console.log("NEXT SWEARING IS AT", video_id["data"][1])
+      //     console.log("NEXT SWEARING DURATION LASTS FOR", video_id["data"][0])
+
+      //     // This way of doing it - will -not- work with REWINDING
+      //     if (currentTimestamp > video_id["data"][1] && currentTimestamp < video_id["data"][0]) {
+      //       console.log("SWEARING INCOMING TIMSTAMP:::::", currentTimestamp);
+      //       console.log("FIRST MUTEEEEEEEEEEEEEEEEEE")
+      //       player.mute();
+      //       video_id["data"].shift();
+      //       video_id["data"].shift();
+      //     } else { console.log("NO SWEARING::::", currentTimestamp) }
+      // }, 500, player);
     }
+
 
     var done = false;
     function onPlayerStateChange(event) {
+
       // Default code //
       var videoStatuses = Object.entries(window.YT.PlayerState);
       console.log(videoStatuses.find(status => status[1] === event.data)[0]);
       // Default code //
 
+      // MUTE doesn't work in my own function even when passed player.
+
+      setInterval(function () {
+
+        // CANT mute here
+        // CAN get timestamp here
+
+        var currentTimestamp = player.playerInfo.currentTime;
+        var endSwearingDuration = video_id["data"][1] + video_id["data"][0];
+
+        if (currentTimestamp > video_id["data"][1] && currentTimestamp < endSwearingDuration) {
+          console.log("SWEARING INCOMING TIMSTAMP:::::", currentTimestamp);
+          console.log("___________________________FIRST MUTEEEEEEEEEEEEEEEEEE")
+          console.log("_____________________________________________")
+
+          video_id["data"].shift();
+          video_id["data"].shift();
+        } else {
+          console.log("NO SWEARING::::", currentTimestamp)
+          console.log("NO SWEARING NEXT SWEAR STARTS AT", video_id["data"][1])
+          console.log("NO SWEARING NEXT SWEAR DURATION IS", video_id["data"][0])
+
+        }
+
+
+      }, 800);
+
+      // try {
+      //   console.log("CURRENT TIME STAMP IS BLINKBLINKBLINKBLINK", player.playerInfo.currentTime)
+      // } catch (err) {
+      //   console.log("ERROR IS", err)
+      // }
+
+
       if (event.data == window.YT.PlayerState.PLAYING && !done) {
-        // var currentTimestamp = 0;
-        // var startSwearing = 67.32;
-        // var durationSwearing = 12.24;
-        var currentTimestamp, startSwearing, durationSwearing;
-        var endSwearing = startSwearing + durationSwearing;
 
-        // console.log("33333333333DATA3333333333", video_id["data"])
+        // CANT mute here
+        // TIMESTAMP is not updated every second here
 
-        var counter = 0;
-        video_id["data"].map((eachSwearPoint) => {
-          if (counter % 2 == 0) {
-            // even number
-            console.log("SWEAR DURATION", eachSwearPoint)
-          }
-          else {
-            // odd number
-            console.log("SWEAR END", eachSwearPoint)
-          }
-          counter++;
-        })
-
-        // console.log("JSON___________", video_id["data"]);
-
-        // var jsonData = JSON.stringify(video_id["data"]);
-        // console.log("JSON PARSED___________", jsonData);
-
-        // console.log("LENGTH", video_id["data"].length)
-        setInterval(function () {
-          currentTimestamp = player.playerInfo.currentTime
-
-
-
-
-          if (currentTimestamp > startSwearing && currentTimestamp < endSwearing) { // HARD CODED
-            console.log("SWEARING INCOMING TIMSTAMP:::::", currentTimestamp);
-            player.seekTo(durationSwearing);
-          } else { console.log("NO SWEARING::::", currentTimestamp) }
-        }, 500);
-
+        // try {
+        //   console.log("CURRENT TIME STAMP IS FROSTFROSTFROSTFROSTFROST", player.playerInfo.currentTime)
+        // } catch (err) {
+        //   console.log("ERROR IS", err)
+        // }
 
         done = true;
       }
@@ -211,4 +261,25 @@ function LoadVideo(video_id, data) {
   );
 }
 
+
+// var currentTimestamp = 0;
+// var startSwearing = 67.32;
+// var durationSwearing = 12.24;
+
+// var counter = 0;
+// video_id["data"].map((eachSwearPoint) => {
+//   if (counter % 2 == 0) {
+//     // even number
+//     console.log("SWEAR DURATION", eachSwearPoint)
+//   }
+//   else {
+//     // odd number
+//     console.log("SWEAR START", eachSwearPoint)
+//   }
+//   counter++;
+// })
+
 export { LoadVideo, IntialPage };
+
+
+
