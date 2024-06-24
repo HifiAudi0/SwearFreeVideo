bbc39934131fe38cf1d079da2c775c31bb8d447e
HifiAudi0 <77525153+HifiAudi0@users.noreply.github.com>
2024-06-24 17:51:59 -0400

    Added google tags back in. Created a index file for when running locally (not on AWS) confirmed still working on AWS

---
+++ index-local.js
@@ -0,0 +1,26 @@
+const https = require('https');
+const fs = require('fs');
+const app = require("./app")
+
+
+
+const port = 51655;
+const server = app.listen(port, () => {
+    console.log("App is running on port", port)
+});
+
+
+// serve the API with signed certificate on 443 (SSL/HTTPS) port
+
+/*
+const httpsServer = https.createServer({
+    key: fs.readFileSync('/etc/letsencrypt/archive/swearfreevideos.com/privkey1.pem'),
+    cert: fs.readFileSync('/etc/letsencrypt/archive/swearfreevideos.com/fullchain1.pem'),
+}, app);
+
+httpsServer.listen(51655, () => {
+    console.log('HTTPS Server running on port 51655');
+});
+
+
+*/
